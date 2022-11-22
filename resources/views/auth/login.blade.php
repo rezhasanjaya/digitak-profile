45cab58a24017a3b4facf491c0d755ed86327442
rezhasanjaya04 <rezhasanjaya04@gmail.com>
2022-11-22 21:36:42 +0700

    First-Commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.auth')
+
+@section('main-content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">{{ __('Login') }}</h1>
+                                </div>
+
+                                @if ($errors->any())
+                                    <div class="alert alert-danger border-left-danger" role="alert">
+                                        <ul class="pl-4 my-2">
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+
+                                <form method="POST" action="{{ route('login') }}" class="user">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-user" name="email" placeholder="{{ __('E-Mail Address') }}" value="{{ old('email') }}" required autofocus>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user" name="password" placeholder="{{ __('Password') }}" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox small">
+                                            <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="custom-control-label" for="remember">{{ __('Remember Me') }}</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-dark btn-user btn-block">
+                                            {{ __('Login') }}
+                                        </button>
+                                    </div>
+
+                    
+                                </form>
+
+                         
+
+                                @if (Route::has('password.request'))
+                                    <div class="text-center">
+                                        <a style="color: orange" class="small" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Password?') }}
+                                        </a>
+                                    </div>
+                                @endif
+
+                                @if (Route::has('register'))
+                                    <div class="text-center">
+                                        <a  style="color: orange" class="small" href="{{ route('register') }}">{{ __('Create an Account!') }}</a>
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
