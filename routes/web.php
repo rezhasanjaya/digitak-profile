4cc0fd1fbbc308be2248aa4948589ce36f7a3544
rezhasanjaya04 <rezhasanjaya04@gmail.com>
2022-11-24 22:13:00 +0700

    Routes Update

--- routes/web.php
+++ routes/web.php
@@ -17,31 +17,18 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
-Auth::routes();
 Route::middleware('auth')->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
     Route::get('/home', 'HomeController@index')->name('home');
-    Route::get('/portofolio', 'PortofolioController@index')->name('portofolio');
+
     Route::get('/profile', 'ProfileController@index')->name('profile');
     Route::put('/profile', 'ProfileController@update')->name('profile.update');
-    Route::resource('user', UserController::class);
+
     Route::get('/about', function () {
         return view('about');
     })->name('about');
-=======
-Route::middleware('auth')->group(function () {
-  Route::get('/', 'HomeController@index')->name('home');
-  Route::get('/home', 'HomeController@index')->name('home');
 
-  Route::get('/profile', 'ProfileController@index')->name('profile');
-  Route::put('/profile', 'ProfileController@update')->name('profile.update');
-
-  Route::get('/about', function () {
-      return view('about');
-  })->name('about');
-
-  Route::resource('user', UserController::class);
-  Route::resource('magang', MagangController::class);
-  Route::get('/magang/skill', 'MagangController@skill')->name('magang.skill');
->>>>>>> c93b9504a565b69cbe0edcdfacefec45b032891a
+    Route::resource('user', UserController::class);
+    Route::resource('magang', MagangController::class);
+    Route::get('/magang/skill', 'MagangController@skill')->name('magang.skill');
 });
