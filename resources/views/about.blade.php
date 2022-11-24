c4f5d41fc3f40a244f12eca075ed1e47e8705d75
rezhasanjaya04 <rezhasanjaya04@gmail.com>
2022-11-24 23:21:34 +0700

    Sedikit Perubahan Logo

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,7 +11,7 @@
             <div class="card shadow mb-4">
 
                 <div class="card-profile-image mt-4">
-                    <img src="{{ asset('img/digitak-icon.png') }}" alt="user-image">
+                    <img src="{{ asset('img/digitak.png') }}" alt="user-image">
                 </div>
 
                 <div class="card-body">
