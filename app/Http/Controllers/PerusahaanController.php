c52979a04aa65824f11d30d0e8f8e6412192817b
rezhasanjaya04 <rezhasanjaya04@gmail.com>
2023-01-26 17:18:55 +0700

    order belum jalan

--- app/Http/Controllers/PerusahaanController.php
+++ app/Http/Controllers/PerusahaanController.php
@@ -11,7 +11,6 @@ class PerusahaanController extends Controller
     public function index()
     {
         $data = Perusahaan::join("users", function ($join) {
-            $join->on("users.id", "=", "perusahaan.edited_by");
             $join->on("users.id", "=", "perusahaan.edited_by")
                 ->orderBy('perusahaan_id', 'desc');
         })->get();
