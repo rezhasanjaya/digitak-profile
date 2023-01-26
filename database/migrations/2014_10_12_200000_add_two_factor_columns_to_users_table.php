215b1ffa5d6ccbb99627fba7f5b637037b1efc16
adimuhamad <adi.10119253@mahasiswa.unikom.ac.id>
2023-01-26 14:01:08 +0700

    Update migrations

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -21,6 +21,7 @@ class AddTwoFactorColumnsToUsersTable extends Migration
             $table->text('two_factor_recovery_codes')
                     ->after('two_factor_secret')
                     ->nullable();
+            $table->engine = "InnoDB";
         });
     }
 
