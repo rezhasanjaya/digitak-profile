2015be7c7edf93fc8a08ef67422e934f3d211798
adimuhamad <adi.10119253@mahasiswa.unikom.ac.id>
2023-01-26 20:34:13 +0700

    Update

--- app/Http/Controllers/PerusahaanController.php
+++ app/Http/Controllers/PerusahaanController.php
@@ -11,16 +11,15 @@ class PerusahaanController extends Controller
     public function index()
     {
         $data = Perusahaan::join("users", function ($join) {
-            $join->on("users.id", "=", "perusahaan.edited_by")
-                ->orderBy('perusahaan_id', 'desc');
-        })->get();
+            $join->on("users.id", "=", "perusahaan.edited_by");
+        })->orderBy('id_prshn', 'desc')->paginate(10);
 
         return view('perusahaan.index', [
             'title' => 'Ubah Data Perusahaan dan Histori',
             'users' => User::all(),
             'perusahaan' => Perusahaan::latest('id_prshn')->first()->orderBy('id_prshn', 'desc')->paginate(10),
-            // 'perusahaan' => Perusahaan::all(),
             'data' => $data,
+            // 'perusahaan' => Perusahaan::latest('id_prshn')->first()->orderBy('id_prshn', 'desc')->paginate(10),
         ]);
     }
 
