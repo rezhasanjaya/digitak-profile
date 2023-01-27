2d4d6e96520fe80c630a8df5c7a43196776c2974
adimuhamad <adi.10119253@mahasiswa.unikom.ac.id>
2023-01-28 05:01:55 +0700

    Update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,8 +8,11 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+    <!-- Bootstrap Carousel -->
+    <link rel="stylesheet" href="https://getbootstrap.com/docs/5.1/examples/carousel/carousel.css">
+
     <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">    
 
     <!-- My Style -->
     <link rel="stylesheet" href="/css/style.css">
