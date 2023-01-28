97864c1f1ec0dfb304dbc21de236cb4ea24b522f
rezhasanjaya04 <rezhasanjaya04@gmail.com>
2023-01-28 07:29:55 +0700

    Update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,59 +1,58 @@
+<style>
+    .p{
+        text-align: justify;;
+        word-wrap: break-word;
+    }
+</style>
+
 <div class="container">
   <footer class="py-5">
     <div class="row">
-      <div class="col-2">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-        </ul>
-      </div>
-
-      <div class="col-2">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-        </ul>
-      </div>
-
-      <div class="col-2">
-        <h5>Section</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
-        </ul>
-      </div>
-
-      <div class="col-4 offset-1">
-        <form>
-          <h5>Subscribe to our newsletter</h5>
-          <p>Monthly digest of whats new and exciting from us.</p>
-          <div class="d-flex w-100 gap-2">
-            <label for="newsletter1" class="visually-hidden">Email address</label>
-            <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
-            <button class="btn btn-primary" type="button">Subscribe</button>
+        <div class="col-4">
+            <h5>Kontak Kami</h5>
+            <hr>
+            <h6><b>DIGITAL DATA STUDIO (DIGITAK)</b></h6>
+                <b>Workshop</b> : $alamat
+                <br>
+                <br>
+                <b>Phone</b>    : $phone
+                <br>
+                <b>WhatsApp</b> : $phone
+                <br>
+                <b>Email</b> : $phone
           </div>
-        </form>
+      <div class="col-4 ">
+        <h5>Ikuti Kami</h5>
+          <hr>
+          <ul class="list-unstyled d-flex">
+            <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+            <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+            <li class="ms-3 mb-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
+          </ul>
+        <h5>Sitemap</h5>
+        <hr>
+        <ul class="nav flex-column">
+          <b><li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Beranda</a></li></b>
+          <b><li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Tentang Kami</a></li></b>
+          <b><li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Portofolio</a></li></b>
+          <b><li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Kontak Kami</a></li></b>
+         
+        </ul>
       </div>
+
+      <div class="col-4">
+        <h5>Tentang Kami</h5>
+        <hr>
+        <h6><b>DIGITAL DATA STUDIO (DIGITAK)</b></h6>
+            <p style="text-align: justify;">adalah sebuah team pengembang perangkat lunak yang fokus pada pengembangan Teknologi Informasi untuk menunjang aktifitas instansi atau perusahaan anda sejak 2014. Kami menciptakan solusi (aplikasi, web design & development dan digital marketing) yang lebih futuris dan pastinya kompatibel dengan kebutuhan perusahaan Anda.</p>
+            
+      </div>
+
+      
     </div>
 
-    <div class="d-flex justify-content-between py-4 my-4 border-top">
+    <div class="d-flex justify-content-between my-3 border-top">
       <p>&copy; 2021 Company, Inc. All rights reserved.</p>
-      <ul class="list-unstyled d-flex">
-        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
-        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
-        <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
-      </ul>
     </div>
   </footer>
 </div>
